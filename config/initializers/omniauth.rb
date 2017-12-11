3677a46bde42a8762b51927a6a9f10f9a119eeb9
igor <zawgorodskiy.2012@gmail.com>
2017-12-11 18:50:32 +0200

    x

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  callback_url: "https://glacial-sands-69857.herokuapp.com/users/auth/facebook/callback"
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
