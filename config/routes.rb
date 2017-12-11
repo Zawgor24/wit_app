262fb3d92993371234d87a057bcc3c7e4d6ee11a
igor <zawgorodskiy.2012@gmail.com>
2017-12-11 20:43:15 +0200

    added logout path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'omniauth#create'
+  
+  delete '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   resources :users do
     resources :messages
